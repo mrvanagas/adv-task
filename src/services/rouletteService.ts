6e2e5c1f7d3256d19a863c549ab385c1fa2c7a69
Lukas <lukonelis1@gmail.com>
2024-04-03 23:22:45 +0300

    creating initial fetch functions, css styles

---
+++ src/services/rouletteService.ts
@@ -0,0 +1,44 @@
+import type { ResultStatList, Spin, WheelConfiguration } from '@/types/models'
+
+const BASE_URL = 'https://dev-games-backend.advbet.com/v1/ab-roulette'
+
+const fetchSpin = async (wheelId: string, uuid: string): Promise<Spin> => {
+  const response = await fetch(`${BASE_URL}/${wheelId}/game/${uuid}`)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch spin')
+  }
+
+  return response.json()
+}
+
+const fetchSpinHistory = async (wheelId: string, limit = 10): Promise<Spin[]> => {
+  const response = await fetch(`${BASE_URL}/${wheelId}/history?limit=${limit}`)
+  if (!response.ok) {
+    throw new Error('Failed to fetch spin history')
+  }
+  return response.json()
+}
+
+const fetchWheelConfiguration = async (wheelId: string): Promise<WheelConfiguration> => {
+  const response = await fetch(`${BASE_URL}/${wheelId}/configuration`)
+  if (!response.ok) {
+    throw new Error('Failed to fetch wheel configuration')
+  }
+  return response.json()
+}
+
+const fetchStatistics = async (wheelId: string): Promise<ResultStatList[]> => {
+  const response = await fetch(`${BASE_URL}/${wheelId}/stats`)
+  if (!response.ok) {
+    throw new Error('Failed to fetch stats')
+  }
+  return response.json()
+}
+
+export const rouletteService = {
+  fetchSpin,
+  fetchSpinHistory,
+  fetchWheelConfiguration,
+  fetchStatistics
+}
---
+++ src/types/models.ts
@@ -0,0 +1,35 @@
+export interface Spin {
+  id: string
+  startTime: string
+  time: string
+  startDelta: Number
+  startDeltaUs: Number
+  fakeStartDelta: Number
+  duration: Number
+  result: Number
+  outcome: string
+}
+
+export interface WheelConfiguration {
+  name: string
+  slots: number
+  results: string[]
+  colors: ('red' | 'black' | 'green')[]
+  positionToId: number[]
+}
+
+export interface ResultStatList {
+  result: Number
+  count: Number
+}
+;[]
+
+export interface ResultStat {
+  result: Number
+  count: Number
+}
+
+export interface ActionLogEntry {
+  timestamp: Date;
+  action: string;
+}
